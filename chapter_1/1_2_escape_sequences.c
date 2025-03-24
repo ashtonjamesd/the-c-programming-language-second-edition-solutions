55d50fe0f51e7c33fc4b8883db25b1ba502eaec8
Ashton Dunderdale <ashtondunderdale@gmail.com>
2025-03-24 09:25:02 +0000

    escape sequences

---
+++ chapter_1/1_2_escape_sequences.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("tab: '\t'\n");
+    printf("backspace: '\b'\n");
+    printf("double quote: '\"'\n");
+    printf("back slash: '\\'\n");
+    return 0;
+}
