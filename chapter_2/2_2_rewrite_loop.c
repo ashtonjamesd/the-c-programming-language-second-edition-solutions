3b2db8ee1f45c9dfe84c3d907ef12b044707772b
rxgq <ashtondunderdale@gmail.com>
2025-03-30 14:03:08 +0100

    rewrite loop

---
+++ chapter_2/2_2_rewrite_loop.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+  int c, i, lim;
+  char s[1];
+  
+  for (i = 0; i < lim - 1; ++i) {
+    if ((c = getchar()) == '\n') break;
+    if (c == EOF) break;
+
+    s[i] = c;
+  }
+    
+
+  return 0;
+}
