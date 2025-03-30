7b706b1b2ae21919343e3f743db5a9a8b7209cd7
rxgq <ashtondunderdale@gmail.com>
2025-03-30 17:05:14 +0100

    any

--- chapter_2/2_5_any.c
+++ chapter_2/2_5_any.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 
-void any() {
-  
+int any(char s1[], char s2[]) {
+  for (int i = 0; s1[i] != '\0'; i++) {
+
+    for (int j = 0; s2[j] != '\0'; j++) {
+      if (s1[i] == s2[j]) {
+        return i;
+      }
+    }
+  }
+
+  return -1;
 }
 
 int main() {
-  
+  char x[20] = "Hello, World!";
+  printf("%d", any(x, "Wrl"));
   return 0;
 }
