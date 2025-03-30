239d050db3a321597c07a77b2f399a1dbff87261
rxgq <ashtondunderdale@gmail.com>
2025-03-30 11:53:44 +0100

    detab

--- chapter_1/1_20_detab.c
+++ chapter_1/1_20_detab.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
 
-int main() {
+#define TAB_STOP 4
+
+void detab(void) {
+  int c, col = 0;
   
+  while (c = getchar() != EOF) {
+    if (c == '\t') {
+      int spaces_to_next_tab = TAB_STOP - (col % TAB_STOP);
+      for (int i = 0; i < spaces_to_next_tab; i++) {
+        putchar(' ');
+      }
+      
+      col += spaces_to_next_tab;
+    }
+    else {
+      putchar(c);
+      col++;
+
+      if (c == '\n') col = 0;
+    }
+  }
+}
+
+int main() {
+  detab();
   return 0;
 }
---
+++ chapter_1/1_21_entab.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#define TAB_STOP 4
+
+void entab(void) {
+  int c;
+  
+  while (c = getchar() != EOF) {
+    
+  }
+}
+
+int main() {
+  entab();
+  return 0;
+}
